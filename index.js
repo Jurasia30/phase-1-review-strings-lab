b501e81ddf2318b4acdc769dabb25e4df2c4d47f
Jurasia <jurasiabritton@gmail.com>
2023-01-06 16:55:56 -0500

    commit tree 5bda0ea2cce2b16a9da9640a48ef6f3bebcd3f97

--- index.js
+++ index.js
@@ -1,1 +1,9 @@
 // Write your code in this file!
+const currentUser = "Asia";
+
+const welcomeMessage = "Welcome to Flatbook, " + 'Asia!';
+
+const excitedWelcomeMessage = welcomeMessage.toUpperCase();
+
+
+const shortGreeting = `Welcome, ${currentUser.slice(0,1)}!`;
